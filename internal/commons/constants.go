9626de2901c18d4bbbd6bb52e04bebee6e94b12d
Daniel Havir <havir.daniel@gmail.com>
2019-05-26 20:01:17 -0700

    Add comments, change HPKE Mode to use ChaCha20Poly1305.

--- internal/commons/constants.go
+++ internal/commons/constants.go
@@ -3,7 +3,7 @@ package commons
 import hpke "github.com/danielhavir/go-hpke"
 
 // HpkeMode specifies ciphersuite
-const HpkeMode = hpke.BASE_X25519_SHA256_XChaCha20Blake2bSIV
+const HpkeMode = hpke.BASE_X25519_SHA256_ChaCha20Poly1305
 
 // BufferSize specifies buffer during file exchange
 const BufferSize = 4096
--- internal/commons/utils.go
+++ internal/commons/utils.go
@@ -7,28 +7,33 @@ import (
 	"golang.org/x/crypto/blake2b"
 )
 
+// DecodeHex decodes a hex-encoded byte array
 func DecodeHex(src []byte) []byte {
 	dst := make([]byte, hex.DecodedLen(len(src)))
 	hex.Decode(dst, src)
 	return dst
 }
 
+// EncodeHex encodes a byte array
 func EncodeHex(src []byte) []byte {
 	dst := make([]byte, hex.EncodedLen(len(src)))
 	hex.Encode(dst, src)
 	return dst
 }
 
+// Hash for hashing a byte array using blake2b
 func Hash(data []byte) []byte {
 	h := blake2b.Sum256(data)
 	return h[:]
 }
 
+// ByteToUint32 converts a byte array into a uint32 number
 func ByteToUint32(in []byte) (out uint32) {
 	out = binary.BigEndian.Uint32(in)
 	return
 }
 
+// Uint32ToByt3 converts a uint32 into a byte array
 func Uint32ToByte(in uint32) (out []byte) {
 	out = make([]byte, 4)
 	binary.BigEndian.PutUint32(out, in)
